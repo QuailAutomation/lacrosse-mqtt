2dd888226a194cabfdf7abaef7fb95cf3e59996d
Craig Hamilton <craigh+git@quailholdings.com>
2016-01-21 20:44:34 -1000

    get working using a config file for sensors

--- twisted-lcr-mqtt.py
+++ twisted-lcr-mqtt.py
@@ -3,6 +3,8 @@ __author__ = 'craigh'
 import os
 #from logentries import LogentriesHandler
 import logging
+import json
+
 from twisted.protocols.basic import LineReceiver
 
 from twisted.internet import reactor
@@ -16,6 +18,7 @@ from twisted.python import log as twisted_log
 
 
 # read some env variables
+config_file = os.getenv('SENSORCONFIGFILE', './sensors.json')
 serial_port = os.getenv('JEELINKUSBPORT', '/dev/ttyUSB0')
 mosquitto_url = os.getenv('MQTTBROKERURL', '192.168.1.122')
 # TODO this needs to be converted to some map config file with device id, topic
@@ -23,17 +26,12 @@ mosquitto_topic = os.getenv('MOSQUITTOTOPIC', 'sensors/garage-south/temperature'
 logentries_key = os.getenv('logentries-key', '9401ac1a-b3ba-45bf-8b48-df0fe1ecd5b0')
 
 
-class THOptions(usage.Options):
-    optParameters = [
-        ['baudrate', 'b', 57600, 'Serial baudrate'],
-        ['port', 'p', serial_port, 'Serial port to use'],]
-
-class ProcessTempSensor(LineReceiver):
-    debug = True
-    mqttc = mqtt.Client('python_pub')
-    deviceToTopicMap = {'80': mosquitto_topic}
-
-    last10Readings = deque(maxlen=10)
+class TempSensor:
+    def __init__(self, topic, min, max):
+        self.topic = str(topic)
+        self.min = min
+        self.max = max
+        self.last10Readings = deque(maxlen=10)
 
     def sma(self):
         sumSamples = sum(self.last10Readings)
@@ -45,8 +43,7 @@ class ProcessTempSensor(LineReceiver):
             return None
         else:
             avg = sumSamples/numSamples;
-            if self.debug:
-                logging.debug('Avg %f' % avg)
+            logging.debug('Avg %f' % avg)
             return avg
 
     def submitSample(self, sample):
@@ -59,11 +56,30 @@ class ProcessTempSensor(LineReceiver):
                 self.last10Readings.append(sample)
                 log.debug( 'Submitted sample')
         else:
-            if 10.0 <= sample <= 35:
+            #TODD get these from config map
+            if self.min <= sample <= self.max:
                 self.last10Readings.append(sample)
                 log.debug( 'Submitted sample')
             else:
                 log.debug( 'Sample out of range')
+
+    def topic(self):
+        return self.topic
+
+    def remove(self, count):
+        for x in range(count):
+            self.last10Readings.popleft()
+
+class THOptions(usage.Options):
+    optParameters = [
+        ['baudrate', 'b', 57600, 'Serial baudrate'],
+        ['port', 'p', serial_port, 'Serial port to use'],]
+
+class ProcessTempSensor(LineReceiver):
+    debug = True
+    mqttc = mqtt.Client('python_pub')
+#    deviceToTopicMap = {'80': mosquitto_topic}
+
 
     def lineReceived(self, line):
         try:
@@ -71,22 +87,23 @@ class ProcessTempSensor(LineReceiver):
             log.info(msg)
             if msg.startswith('D:') and len(msg) >= 13:
                 msgElements = msg.split(':')
-               # log.debug('Elements: ', str(msgElements))
                 key = msgElements[1]
                 log.debug('key: %s' % key)
                 temp = float(msgElements[2])
                 log.debug("temp: '%f'" % temp)
                 try:
-                    topic = self.deviceToTopicMap[key]
-                    self.submitSample(temp)
-                    sma = self.sma()
+                    configInfo = deviceToTopicMap[key]
+                    print("Config info is: %s" %  configInfo)
+                    topic = configInfo.topic
+                    print("Topic is :%s"% topic)
+                    configInfo.submitSample(temp)
+                    sma = configInfo.sma()
                     if sma is not None:
                         log.debug( 'Writing to topic: ' + topic)
                         self.mqttc.connect(mosquitto_url, 1883, keepalive=1000)
                         self.mqttc.publish(topic, str(sma))
                         # let's remove 5 oldest readings so we can build deque back to 10
-                        for x in xrange(5):
-                            self.last10Readings.popleft()
+                        configInfo.remove(5)
                     else:
                         log.debug( 'Did not write sma because is None')
                     #mqttc.loop(2)
@@ -112,6 +129,19 @@ def SerialInit():
     reactor.run()
 
 print 'Starting'
+
+with open(config_file) as json_file:
+    sensorConfig = json.load(json_file)
+
+# for each id, let's create a dict with the id, and a temp sensor cloass
+#TempSensor
+deviceToTopicMap = {}
+for key in sensorConfig:
+    topic = sensorConfig[key]['mqtt-topic']
+    print ('Topic: ',topic)
+    min = sensorConfig[key]['valid-range']['min']
+    print ('Min: ', min)
+    deviceToTopicMap[key] = TempSensor(sensorConfig[key]['mqtt-topic'],sensorConfig[key]['valid-range']['min'],sensorConfig[key]['valid-range']['max'])
 
 #log = logging.getLogger('logentries')
 #log.setLevel(logging.INFO)
