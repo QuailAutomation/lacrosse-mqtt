1d52b4de8d9e221da7b26e638af87a625e0c8390
Craig <craigh@quailholdings.com>
2018-08-07 08:09:17 -0700

    clean up around oo.  need to test now

--- monitor-mqtt.py
+++ monitor-mqtt.py
@@ -1,34 +1,21 @@
-import time
 import os
 import json
-import paho.mqtt.client as mqtt
 import logging
-import socket
-import datetime
+
 import thread
-from flask import Flask, jsonify, Response
-from prometheus_client import Summary, Counter, Gauge, generate_latest
-from sensors import TempSensor
+from flask import Flask, Response
+from prometheus_client import generate_latest
+from sensors import TempSensor, MqttMonitor
 try:
     import graypy
 except ImportError:
     pass
 
 
-
 app = Flask(__name__)
 
 # instrumentation
 CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
-SENSOR_SAMPLES = Counter('lacrosse_samples_submitted', 'Number of samples processed', ['sensor_key','topic','location'])
-INVALID_SENSOR_RANGE_COUNTER = Counter('lacrosse_samples_invalid_range_submitted', 'Number unknown key samples processed', ['sensor_key','topic'])
-INVALID_SENSOR_KEY_COUNTER = Counter('lacrosse_samples_invalid_key_submitted', 'Number unknown key samples processed', ['sensor_key'])
-MQTT_SUBMIT_DURATION = Summary('lacrosse_mqtt_submit_duration',
-                           'Latency of submitting to mqtt')
-MQTT_EXCEPTIONS = Counter('lacrosse_mqtt_submit_exceptions_total',
-                             'Exceptions thrown submitting to mqtt')
-CURRENT_NUMBER_SAMPLES = Gauge('lacrosse_samples_current_number', 'Current number samples for averaging', ['sensor_key', 'type','location'])
-
 
 config_file = os.getenv('SENSORCONFIGFILE', './sensors.json')
 with open(config_file) as json_file:
@@ -44,9 +31,10 @@ try:
 except KeyError:
     gelf_url = None
 
-# if we have log configuration for log servers, add that, otherwise let's use basic loggin
+# if we have log configuration for log servers, add that, otherwise let's use basic logging
 isLogConfigInfo = False
 
+#TODO should test setting the url, but not having graypy avail as lib
 if gelf_url is not None:
     handler = graypy.GELFHandler(gelf_url, 12201, localname='lacrosse-processing', facility="maui")
     log.addHandler(handler)
@@ -58,152 +46,19 @@ if not isLogConfigInfo:
 logging_level = config['log-level'] or "INFO"
 log.setLevel(level=logging_level or logging.INFO)
 
-# for each id, let's create a dict with the id, and a temp sensor class
-device_id_to_temp_sensor_map = {}
-device_id_to_humidity_sensor_map = {}
 
-# this dict is sensors id, mqtt topic to write to
-deviceIdtoTopic = {}
-
-sensorConfig = config['sensors']
-for key in sensorConfig:
-    log.info("watching for sensor with id: %s" % key)
-    topic = sensorConfig[key]['mqtt-topic']
-    try:
-        location = sensorConfig[key]['location']
-    except KeyError:
-        location = topic
-
-    log.info('Topic: %s' % topic)
-    min = sensorConfig[key]['valid-range']['min']
-    device_id_to_temp_sensor_map[key] = TempSensor(key,location,sensorConfig[key]['valid-range']['min'], sensorConfig[key]['valid-range']['max'])
-    device_id_to_humidity_sensor_map[key] = TempSensor(id=key,location=location,min=0, max=97, max_difference_from_average=15)
-    deviceIdtoTopic[key] = topic
-
-
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    client.subscribe("private/jeelink/+")
-
-# this dict will keep track of last time we received a message for the sensors.
-# more than 1 jeelink may pick up the reading from a temp sensor, so igore readings
-# within a recent window...ie within 3 seconds
-device_id_to_last_reading_time = {}
-
-
-# if i we reject 5 entries in a
-def is_ok_to_accept_reading(key):
-    return_value = True
-    log.debug("checking if we've had a reading within the ingore window")
-    now = datetime.datetime.now()
-    try:
-        time_last_reading = device_id_to_last_reading_time[key]
-        elapsed_time = now  - time_last_reading
-        if elapsed_time.total_seconds() > 3:
-            return_value = True
-            log.debug("we can accept, elapsed time: %f" % elapsed_time.total_seconds())
-        else:
-            return_value = False
-    except KeyError:
-        log.info('Key not found in last reading: ' + key)
-
-    device_id_to_last_reading_time[key] = datetime.datetime.now()
-    return return_value
-
-
-def get_sensor(key, type):
-    try:
-        if type == 'temperature':
-            return device_id_to_temp_sensor_map[key]
-        elif type == 'humidity':
-            return device_id_to_humidity_sensor_map[key]
-        else:
-            log.error('Illegal sensor type: "%s"' % type)
-    except KeyError:
-        log.info('Key not found: %s, type: %s' % (key,type))
-
-
-def write_to_mqtt(topic, value):
-    log.info('Writing to topic: %s, val: %s' % (topic, str(value)))
-    try:
-        startTime = time.time()
-        client.publish(topic, str(value))
-        MQTT_SUBMIT_DURATION.observe(time.time() - startTime)
-    except socket.error:
-        log.warn('Could not connect to mosquitto')
-        MQTT_EXCEPTIONS.inc()
-        client.connect(mosquitto_url, 1883, keepalive=1000)
-
-
-def submit_sample(sensor, sample_value, topic, type):
-    log.debug("submit sample called :" + str(sample_value))
-    try:
-        sensor.submit_sample(sample_value)
-        sma = sensor.sma()
-        if sma is not None:
-            if type == 'temperature':
-                write_to_mqtt(topic + type, round(sma,1))
-            elif type == 'humidity':
-                write_to_mqtt(topic + type, round(sma,0))
-            else:
-                log.warn("Received unexpected sample type: %s" % type)
-
-            # let's remove 5 oldest readings so we can build deque back to 10
-            sensor.remove(5)
-        else:
-            log.debug('Did not write sma because is None')
-    except ValueError:
-        log.info('Invalid reading. Sample ignored. sensor: {}, value={}'.format(sensor,sample_value))
-
-
-def on_message(client, userdata, msg):
-    payload = msg.payload
-    log.debug("payload: %s" % payload)
-    msgElements = payload.split(':')
-    key = msgElements[1]
-    log.debug('key: %s' % key)
-    if is_ok_to_accept_reading(key):
-        temp = float(msgElements[2])
-        log.debug("temp: '%f'" % temp)
-        label_dict = {"sensor_key": key,"topic":msg.topic}
-        sensor = get_sensor(key, 'temperature')
-        if sensor is not None:
-            label_dict['location'] = sensor.location
-            SENSOR_SAMPLES.labels(**label_dict).inc()
-            topic = deviceIdtoTopic[key]
-            submit_sample(sensor, temp, topic, 'temperature')
-            CURRENT_NUMBER_SAMPLES.labels(sensor_key=key, type= 'temperature',location=sensor.location).set(sensor.number_samples())
-            log.debug('Submitted temperature reading. key=%s value=%f' % (key, temp))
-            humidity = float(msgElements[3])
-            sensor = get_sensor(key, 'humidity')
-            if humidity < 99:
-                log.debug("humidity: '%f'" % humidity)
-                submit_sample(sensor, humidity, topic, 'humidity')
-                CURRENT_NUMBER_SAMPLES.labels(sensor_key=key, type='humidity', location=sensor.location).set(sensor.number_samples())
-        else:
-            INVALID_SENSOR_KEY_COUNTER.labels(sensor_key=key).inc()
+# used for dumping the dicts, probably could move over to MonitorMQTT class
 class SetEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, TempSensor):
             return obj.toJSON()
         return json.JSONEncoder.default(self, obj)
 
-# add rest interface
-@app.route('/lacrosse/1.0/sensors/temperature', methods=['GET'])
-def get_sensors_temperature():
-    return json.dumps(device_id_to_temp_sensor_map,cls=SetEncoder, sort_keys=True,indent=4, separators=(',', ': ')) #jsonify(device_id_to_temp_sensor_map['4'])
 
-@app.route('/lacrosse/1.0/sensors/humidity', methods=['GET'])
-def get_sensors_humidity():
-    return json.dumps(device_id_to_humidity_sensor_map,cls=SetEncoder, sort_keys=True,indent=4, separators=(',', ': ')) #jsonify(device_id_to_temp_sensor_map['4'])
-
-
-def flaskThread():
+def flask_thread():
      app.run(host='0.0.0.0')
 
-thread.start_new_thread(flaskThread,())
+thread.start_new_thread(flask_thread,())
 
 
 @app.route('/metrics')
@@ -211,14 +66,22 @@ def metrics():
     return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 
-client = mqtt.Client(client_id="lacrosse-mqtt")
-client.on_connect = on_connect
-client.on_message = on_message
-log.info('Connecting to mqtt: %s' % mosquitto_url)
-client.connect(mosquitto_url, 1883, 60)
+mqtt_monitor = MqttMonitor(config['sensors'], mosquitto_url)
+
+
+# add rest interface
+@app.route('/lacrosse/1.0/sensors/temperature', methods=['GET'])
+def get_sensors_temperature():
+    return json.dumps(mqtt_monitor.device_id_to_temp_sensor_map,cls=SetEncoder,
+                      sort_keys=True,indent=4, separators=(',', ': '))
+
+
+@app.route('/lacrosse/1.0/sensors/humidity', methods=['GET'])
+def get_sensors_humidity():
+    return json.dumps(mqtt_monitor.device_id_to_humidity_sensor_map,cls=SetEncoder,
+                      sort_keys=True,indent=4, separators=(',', ': '))
+
 
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
-# Other loop*() functions are available that give a threaded interface and a
-# manual interface.
-client.loop_forever()
+mqtt_monitor.loop_forever()
--- sensors.py
+++ sensors.py
@@ -2,6 +2,12 @@ from collections import deque
 
 import logging
 import json
+import datetime
+import time
+import socket
+
+from prometheus_client import Summary, Counter, Gauge, generate_latest
+import paho.mqtt.client as mqtt
 
 log = logging.getLogger(__name__)
 
@@ -35,7 +41,7 @@ class TempSensor:
             log.debug('SMA is None because # samples is: %s' % len(self.last_10_readings))
             return None
         else:
-            avg = self.average();
+            avg = self.average()
             log.debug('Avg %f' % avg)
             return avg
 
@@ -77,3 +83,155 @@ class TempSensor:
 
     def toJSON(self):
         return {'id': self.id,'min':self.min,'max':self.max,'currentnumberrejections':self.current_number_sample_rejections,'readings':json.dumps(list(self.last_10_readings))}
+
+
+class MqttMonitor:
+    SENSOR_SAMPLES = Counter('lacrosse_samples_submitted', 'Number of samples processed',
+                             ['sensor_key', 'topic', 'location'])
+    INVALID_SENSOR_RANGE_COUNTER = Counter('lacrosse_samples_invalid_range_submitted',
+                                           'Number unknown key samples processed', ['sensor_key', 'topic'])
+    INVALID_SENSOR_KEY_COUNTER = Counter('lacrosse_samples_invalid_key_submitted',
+                                         'Number unknown key samples processed', ['sensor_key'])
+    MQTT_SUBMIT_DURATION = Summary('lacrosse_mqtt_submit_duration',
+                                   'Latency of submitting to mqtt')
+    MQTT_EXCEPTIONS = Counter('lacrosse_mqtt_submit_exceptions_total',
+                              'Exceptions thrown submitting to mqtt')
+    CURRENT_NUMBER_SAMPLES = Gauge('lacrosse_samples_current_number', 'Current number samples for averaging',
+                                   ['sensor_key', 'type', 'location'])
+
+    def __init__(self,sensor_config, mqtt_broker_url, mqtt_broker_port = 1883,
+                 broker_client_id="lacrosse-mqtt"):
+        self.client = mqtt.Client(client_id=broker_client_id)
+        self.mqtt_broker_url = mqtt_broker_url
+        self.mqtt_broker_port = 1883
+
+        self.client.on_connect = self.on_connect
+        self.client.on_message = self.on_message
+
+        # for each id, let's create a dict with the id, and a temp sensor class
+        self.device_id_to_temp_sensor_map = {}
+        self.device_id_to_humidity_sensor_map = {}
+        # this dict will keep track of last time we received a message for the sensors.
+        # more than 1 jeelink may pick up the reading from a temp sensor, so igore readings
+        # within a recent window...ie within 3 seconds
+        self.device_id_to_last_reading_time = {}
+
+        # this dict is sensors id, mqtt topic to write to
+        self.deviceIdtoTopic = {}
+
+        for key in sensor_config:
+            log.info("watching for sensor with id: %s" % key)
+            topic = sensor_config[key]['mqtt-topic']
+            try:
+                location = sensor_config[key]['location']
+            except KeyError:
+                location = topic
+
+            log.info('Topic: %s' % topic)
+            min = sensor_config[key]['valid-range']['min']
+            self.device_id_to_temp_sensor_map[key] = TempSensor(key, location, sensor_config[key]['valid-range']['min'],
+                                                           sensor_config[key]['valid-range']['max'])
+            self.device_id_to_humidity_sensor_map[key] = TempSensor(id=key, location=location, min=0, max=97,
+                                                               max_difference_from_average=15)
+            self.deviceIdtoTopic[key] = topic
+
+    def on_connect(self, client, userdata, flags, rc):
+        print("Connected with result code " + str(rc))
+        # Subscribing in on_connect() means that if we lose the connection and
+        # reconnect then subscriptions will be renewed.
+        client.subscribe("private/jeelink/+")
+
+    # if i we reject 5 entries in a
+    def is_ok_to_accept_reading(self, key):
+        return_value = True
+        log.debug("checking if we've had a reading within the ignore window")
+        now = datetime.datetime.now()
+        try:
+            time_last_reading = self.device_id_to_last_reading_time[key]
+            elapsed_time = now - time_last_reading
+            if elapsed_time.total_seconds() > 3:
+                return_value = True
+                log.debug("we can accept, elapsed time: %f" % elapsed_time.total_seconds())
+            else:
+                return_value = False
+        except KeyError:
+            log.info('Key not found in last reading: ' + key)
+
+        self.device_id_to_last_reading_time[key] = datetime.datetime.now()
+        return return_value
+
+    def get_sensor(self, key, type):
+        try:
+            if type == 'temperature':
+                return self.device_id_to_temp_sensor_map[key]
+            elif type == 'humidity':
+                return self.device_id_to_humidity_sensor_map[key]
+            else:
+                log.error('Illegal sensor type: "%s"' % type)
+        except KeyError:
+            log.info('Key not found: %s, type: %s' % (key, type))
+
+    def write_to_mqtt(self, topic, value):
+        log.info('Writing to topic: %s, val: %s' % (topic, str(value)))
+        try:
+            startTime = time.time()
+            self.client.publish(topic, str(value))
+            MqttMonitor.MQTT_SUBMIT_DURATION.observe(time.time() - startTime)
+        except socket.error:
+            log.warn('Could not connect to mosquitto')
+            MqttMonitor.MQTT_EXCEPTIONS.inc()
+            self.client.connect(self.mqtt_broker_url, self.mqtt_broker_port, keepalive=1000)
+
+    def submit_sample(self, sensor, sample_value, topic, type):
+        log.debug("submit sample called :" + str(sample_value))
+        try:
+            sensor.submit_sample(sample_value)
+            sma = sensor.sma()
+            if sma is not None:
+                if type == 'temperature':
+                   self. write_to_mqtt(topic + type, round(sma, 1))
+                elif type == 'humidity':
+                    self.write_to_mqtt(topic + type, round(sma, 0))
+                else:
+                    log.warn("Received unexpected sample type: %s" % type)
+
+                # let's remove 5 oldest readings so we can build deque back to 10
+                sensor.remove(5)
+            else:
+                log.debug('Did not write sma because is None')
+        except ValueError:
+            log.info('Invalid reading. Sample ignored. sensor: {}, value={}'.format(sensor, sample_value))
+
+    def on_message(self, client, userdata, msg):
+        payload = msg.payload
+        log.debug("payload: %s" % payload)
+        msgElements = payload.split(':')
+        key = msgElements[1]
+        log.debug('key: %s' % key)
+        if self.is_ok_to_accept_reading(key):
+            temp = float(msgElements[2])
+            log.debug("temp: '%f'" % temp)
+            label_dict = {"sensor_key": key, "topic": msg.topic}
+            sensor = self.get_sensor(key, 'temperature')
+            if sensor is not None:
+                label_dict['location'] = sensor.location
+                MqttMonitor.SENSOR_SAMPLES.labels(**label_dict).inc()
+                topic = self.deviceIdtoTopic[key]
+                self.submit_sample(sensor, temp, topic, 'temperature')
+                MqttMonitor.CURRENT_NUMBER_SAMPLES.labels(sensor_key=key, type='temperature', location=sensor.location).set(
+                    sensor.number_samples())
+                log.debug('Submitted temperature reading. key=%s value=%f' % (key, temp))
+                humidity = float(msgElements[3])
+                sensor = self.get_sensor(key, 'humidity')
+                if humidity < 99:
+                    log.debug("humidity: '%f'" % humidity)
+                    self.submit_sample(sensor, humidity, topic, 'humidity')
+                    MqttMonitor.CURRENT_NUMBER_SAMPLES.labels(sensor_key=key, type='humidity', location=sensor.location).set(
+                        sensor.number_samples())
+            else:
+                MqttMonitor.INVALID_SENSOR_KEY_COUNTER.labels(sensor_key=key).inc()
+
+    def loop_forever(self):
+        log.info('Connecting to mqtt: %s' % self.mqtt_broker_url)
+        self.client.connect(self.mqtt_broker_url, self.mqtt_broker_port, 60)
+        self.client.loop_forever()
