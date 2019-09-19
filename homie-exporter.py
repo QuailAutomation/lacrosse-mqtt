6d5805690eac4dfbbb1e4bdce21c4dfe92f66741
craigh <craigh@quailholdings.com>
2019-09-19 09:14:18 -0700

    initial, non working

---
+++ homie-exporter.py
@@ -0,0 +1,14 @@
+class HomieExporter:
+    def __init__(self, sensor,mqtt_broker):
+        self.sensor = sensor
+        self.mqtt = mqtt_broker
+
+    def advertise(self):
+        pass
+
+    def export_reading(self):
+        pass
+
+
+
+
--- homie-test.py
+++ homie-test.py
@@ -1,3 +1,6 @@
+# -*- coding: utf-8 -*-
+
+
 import random
 import time
 import paho.mqtt.client as mqtt
@@ -9,49 +12,84 @@ mqtt_client_keepalive = 60
 
 homie_prefix = "homie"
 homie_version = "3.0.0"
-homie_device_id = "homie-3-temperature-humidity-sensor"
+homie_device_id = "homie-3-temperature-humidity-sensor-basement"
 homie_device_name = "Lacrosse sensor"
 homie_device_ip = "192.168.1.150"
 homie_device_mac = "DE:AD:BE:EF:FE:ED"
 homie_device_nodes = ["temperature", "humidity"]
 homie_device_state = None
 
+homie_lacrosse_sensor_ids = ["C4", "D8"]
+
+degree_sign= u'\N{DEGREE SIGN}'
+
+def publish_info(topic, attribute, value):
+    client.publish("{}/{}".format(topic, attribute), value, 1, True)
+
+
 def publish_homie_version(client):
     topic = "{}/{}/{}".format(homie_prefix, homie_device_id, "$homie")
     client.publish(topic, homie_version, 1, True)
 
-def advertize_device():
-    client.publish("homie/homie-3-temperature-sensor/$state", "init", 1, True)
-    publish_device_info()
-    publish_node_info()
-    client.publish("homie/homie-3-temperature-sensor/$state", "ready", 1, True)
 
-def publish_device_info():
-    client.publish("homie/homie-3-temperature-sensor/$homie", homie_version, 1, True)
-    client.publish("homie/homie-3-temperature-sensor/$name", homie_device_name, 1, True)
-    client.publish("homie/homie-3-temperature-sensor/$localip", homie_device_ip, 1, True)
-    client.publish("homie/homie-3-temperature-sensor/$mac", homie_device_mac, 1, True)
-    client.publish("homie/homie-3-temperature-sensor/$fw/name", "sensor-firmware", 1, True)
-    client.publish("homie/homie-3-temperature-sensor/$fw/version", "1.0.0", 1, True)
-    client.publish("homie/homie-3-temperature-sensor/$nodes", "temperature", 1, True)
-    client.publish("homie/homie-3-temperature-sensor/$implementation", "homie-python", 1, True)
-    client.publish("homie/homie-3-temperature-sensor/$stats/interval", "60", 1, True)
+def advertize_device(lacrosse_code):
+    device_id = "lacrosse-temperature-humidity-sensor-{}".format(lacrosse_code)
+    device_topic = "homie/{}".format(device_id)
+    publish_info(device_topic, "$state", "init")
+    publish_device_info(lacrosse_code)
+    publish_node_info(lacrosse_code)
+    publish_info(device_topic,"$state", "ready")
 
-def publish_node_info():
-    client.publish("homie/homie-3-temperature-sensor/temperature/$name", "Indoor temperature", 1, True)
-    client.publish("homie/homie-3-temperature-sensor/temperature/$type", "Temperature", 1, True)
-    client.publish("homie/homie-3-temperature-sensor/temperature/$properties", "temperature", 1, True)
+def publish_device_info(lacrosse_code):
+    device_id = "lacrosse-temperature-humidity-sensor-{}".format(lacrosse_code)
+    device_topic = "homie/{}".format(device_id)
 
-    client.publish("homie/homie-3-temperature-sensor/temperature/temperature", 23, 1, True)
-    client.publish("homie/homie-3-temperature-sensor/temperature/temperature/$name", "Indoor temperature", 1, True)
-    client.publish("homie/homie-3-temperature-sensor/temperature/temperature/$settable", "false", 1, True)
-    client.publish("homie/homie-3-temperature-sensor/temperature/temperature/$unit", "C", 1, True)
-    client.publish("homie/homie-3-temperature-sensor/temperature/temperature/$datatype", "float", 1, True)
-    client.publish("homie/homie-3-temperature-sensor/temperature/temperature/$format", "-20:120", 1, True)
+    publish_info(device_topic,"$homie", homie_version)
+    publish_info(device_topic, "$name", homie_device_name)
+    publish_info(device_topic, "$localip", homie_device_ip)
+    publish_info(device_topic, "$mac", homie_device_mac)
+    publish_info(device_topic, "$fw/name", "sensor-firmware")
+    publish_info(device_topic, "$fw/version", "1.0.0")
+    publish_info(device_topic, "$nodes", "temperature,humidity")
+    publish_info(device_topic, "$implementation", "homie-python")
+    publish_info(device_topic, "$stats/interval", "60")
+
+def publish_node_info(lacrosse_code):
+    device_id = "lacrosse-temperature-humidity-sensor-{}".format(lacrosse_code)
+    device_topic = "homie/{}/temperature".format(device_id)
+
+    publish_info(device_topic, "$name", "temperature")
+    
+    publish_info(device_topic, "$type", "Temperature")
+    publish_info(device_topic, "$properties", "temperature")
+
+    device_topic = "homie/{}/temperature/temperature".format(device_id)
+    publish_info(device_topic, "temperature", 23)
+    publish_info(device_topic, "$name", "temperature")
+    publish_info(device_topic, "$settable", "false")
+    publish_info(device_topic, "$unit", degree_sign + "C")
+    publish_info(device_topic, "$datatype", "float")
+    publish_info(device_topic, "$format", "-20:120")
+
+    # humidity
+    device_topic = "homie/{}/humidity".format(device_id)
+    publish_info(device_topic, "$name", "humidity")
+    publish_info(device_topic, "$type", "Humidity")
+    publish_info(device_topic, "$properties", "humidity")
+
+    device_topic = "homie/{}/humidity/humidity".format(device_id)
+    publish_info(device_topic, "humidity", 55)
+    publish_info(device_topic, "$name", "humidity")
+    publish_info(device_topic, "$settable", "false")
+    publish_info(device_topic, "$unit", "%")
+    publish_info(device_topic, "$datatype", "float")
+    publish_info(device_topic, "$format", "0:100")
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code {}".format(rc))
-    advertize_device()
+    advertize_device(homie_lacrosse_sensor_ids[0])
+    advertize_device(homie_lacrosse_sensor_ids[1])
 
 
 def on_message(client, userdata, msg):
@@ -63,8 +101,13 @@ def loop():
     while True:
         if (time.time() - last_temperature_time) > 5:
             temperature_value = random.uniform(20,25)
-            print("Sending tempertaure value: {0:.2f}C".format(temperature_value))
-            client.publish("homie/homie-3-temperature-sensor/temperature/temperature", "{0:.2f}".format(temperature_value), 1, True)
+            humidity_value = random.uniform(50,60)
+            print(u"Sending tempertaure value: {0:.2f}{1}C".format(temperature_value,degree_sign))
+            device_id = "lacrosse-temperature-humidity-sensor-{}".format("C4")
+            device_topic = "homie/{}".format(device_id)
+            client.publish("{}/temperature/temperature".format(device_topic), "{0:.2f}".format(temperature_value), 1, True)
+            client.publish("{}/humidity/humidity".format(device_topic),
+                           "{0:.2f}".format(humidity_value), 1, True)
             last_temperature_time = time.time()
 
 
@@ -72,7 +115,9 @@ client = mqtt.Client(mqtt_client_id)
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.will_set("homie/homie-3-temperature-sensor/$state", "lost", 1, True)
+device_id = "lacrosse-temperature-humidity-sensor-{}".format("C4")
+device_topic = "homie/{}/".format(device_id)
+client.will_set("{}/$state".format(device_topic), "lost", 1, True)
 
 client.connect(mqtt_broker_host, mqtt_broker_port, mqtt_client_keepalive)
 
@@ -82,6 +127,6 @@ try:
     loop()
 except KeyboardInterrupt:
     print("Stopping client")
-    client.publish("homie/homie-3-temperature-sensor/$state", "disconnected", 1, True)
+    #client.publish("homie/homie-3-temperature-sensor/$state", "disconnected", 1, True)
     client.disconnect()
     client.loop_stop()
